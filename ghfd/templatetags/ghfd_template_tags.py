04cf726fe56520987f1fda0d9ca64e6949235e5b
Qifeng Wang <2524070w@student.gla.ac.uk>
2021-08-04 23:07:49 +0000

    login logout and regist

--- ghfd/templatetags/ghfd_template_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ghfd.models import Category
-
-register = template.Library()
-
-@register.inclusion_tag('ghfd/categories.html')
-def get_category_list(current_category=None):
-    return {'categories': Category.objects.all(),
-            'current_category': current_category} 
